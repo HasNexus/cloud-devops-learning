5ea066e78682cfb33e0a12c17c59e099324a7653
HasNexus <hassannexus12@gmail.com>
2025-11-22 16:07:46 +0000

    Remove default values from variables in vars.tf
    Removed default values for several variables in vars.tf.

--- Terraform/Azure/Load-balancer-basic/vars.tf
+++ Terraform/Azure/Load-balancer-basic/vars.tf
@@ -1,7 +1,6 @@
 variable "sub_id" {
   description = "The Subscription ID for the Azure Account"
   type        = string
-  default     = "ecb50d31-507e-4df2-8e72-0100c3257230"
 }
 
 variable "prefix" {
@@ -19,17 +18,14 @@ variable "location" {
 variable "ssh_key" {
   description = "Path to the SSH public key"
   type        = string
-  default     = "tf-demo.pub"
 }
 
 variable "my_ip_address" {
   description = "Your IP address to allow SSH access"
   type        = string
-  default     = "152.37.101.89/32"
 }
 
 variable "private_key" {
   description = "Path to the SSH private key"
   type        = string
-  default     = "tf-demo"
 }
